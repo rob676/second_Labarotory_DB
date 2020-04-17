d54b10ab720111b3ff447a8c63a41fd837c8f73f
григоренко катерина <mamalaga63@gmail.com>
2020-04-17 13:01:32 +0300

    create SQL

---
+++ creat.sql
@@ -0,0 +1,20 @@
+CREATE TABLE country(
+country varchar(60) NOT NULL
+,coastline float NULL
+,net_migration float NULL
+,infant_mortality float NULL
+,GDP float NULL
+,literacy float NULL
+,CONSTRAINT country_pk PRIMARY KEY (country)
+);
+
+
+CREATE TABLE region(
+region varchar(70) NOT NULL
+,country varchar(60) NOT NULL
+, CONSTRAINT region_pk 
+PRIMARY KEY (region)
+,CONSTRAINT country_fk 
+FOREIGN KEY (country)
+REFERENCES country (country)
+);
